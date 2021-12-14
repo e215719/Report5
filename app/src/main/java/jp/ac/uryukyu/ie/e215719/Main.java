5747973c8b25e15721354408095d56676354d00d
Ryuki IKEHARA <e215719@ie.u-ryukyu.ac.jp>
2021-12-14 10:41:05 +0900

    ステップ2_例外処理

--- app/src/main/java/jp/ac/uryukyu/ie/e215719/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215719/Main.java
@@ -3,6 +3,11 @@ package jp.ac.uryukyu.ie.e215719;
 public class Main {
     public static void main(String[] args) {
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("ヌルポ(NullPointerException)が発生");
+            System.out.println(e.getMessage());
+        }
     }
 }
