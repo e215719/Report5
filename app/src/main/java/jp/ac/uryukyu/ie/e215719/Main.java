1173c56088fb7eef7782c84171ad0879e9357d64
Ryuki IKEHARA <e215719@ie.u-ryukyu.ac.jp>
2021-12-17 23:03:47 +0900

    ステップ3_NumberFormatException

--- app/src/main/java/jp/ac/uryukyu/ie/e215719/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215719/Main.java
@@ -2,12 +2,7 @@ package jp.ac.uryukyu.ie.e215719;
 
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("ヌルポ(NullPointerException)が発生");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        System.out.println(Integer.parseInt(str));
     }
 }
